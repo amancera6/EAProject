6611c801d52304da33f051f8ed7580af2f1aea1f
Alvaro <Alvaro@172.17.3.128>
2017-08-11 15:05:11 -0500

    First Commit

---
+++ WishShoppingCart/src/main/java/mum/edu/app/MainApp.java
@@ -0,0 +1,17 @@
+package mum.edu.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication 
+@ComponentScan("mum.edu")
+public class MainApp {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(MainApp.class, args);
+
+	}
+
+}
---
+++ WishShoppingCart/src/main/java/mum/edu/controller/HelloController.java
@@ -0,0 +1,16 @@
+package mum.edu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@RequestMapping(value="/hello")
+	public String Hello()
+	{
+		return "Hello";
+	}
+	
+}
